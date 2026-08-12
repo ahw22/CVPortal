bca79fcba7ebdb398e24ddc277a0bc3889074c87
ahwz <andreas.zincke@gmx.at>
2026-08-12 15:08:56 +0200

    implement initial UserRepository

---
+++ backend/src/main/java/at/bbrz/cvportal/backend/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package at.bbrz.cvportal.backend.repositories;
+
+import at.bbrz.cvportal.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
