34fc3884897b9b4795c2fc589746d7691c029e1e
ahwz <andreas.zincke@gmx.at>
2026-08-21 12:59:36 +0200

    implement InvalidCredentialsException and UserAlreadyExistsException

---
+++ backend/src/main/java/at/bbrz/cvportal/backend/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package at.bbrz.cvportal.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException() {
+        super("Benutzername oder Passwort falsch");
+    }
+}
