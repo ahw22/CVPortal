c11501a196eabacbb1ca149b50078a034ebf9a56
ahwz <andreas.zincke@gmx.at>
2026-08-25 16:02:44 +0200

    add missing method to CurriculumVitaeRepository interface

--- backend/src/main/java/at/bbrz/cvportal/backend/repositories/CurriculumVitaeRepository.java
+++ backend/src/main/java/at/bbrz/cvportal/backend/repositories/CurriculumVitaeRepository.java
@@ -4,7 +4,9 @@ import at.bbrz.cvportal.backend.entities.CurriculumVitae;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CurriculumVitaeRepository extends JpaRepository<CurriculumVitae, Long> {
     Optional<CurriculumVitae> findByUserUsername(String username);
+    Optional<CurriculumVitae> findByUserId(UUID userId);
 }
