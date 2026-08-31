76820614c854392b51e8af8629cced30bc5e5546
ahwz <andreas.zincke@gmx.at>
2026-08-31 14:15:53 +0200

    remove trailing comma in json and unneccesary imports

--- backend/src/test/java/at/bbrz/cvportal/backend/controller/WorkExperienceControllerTest.java
+++ backend/src/test/java/at/bbrz/cvportal/backend/controller/WorkExperienceControllerTest.java
@@ -20,7 +20,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -112,7 +111,7 @@ class WorkExperienceControllerTest {
                         .content("""
                                 {
                                     "company":"BBRZ",
-                                    "position":"Applikationsentwickler",
+                                    "position":"Applikationsentwickler"
                                 }
                                 """))
                 .andExpect(status().isBadRequest());
